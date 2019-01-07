7f852d22c0dc3730ba9ff43ee85a129e82058d5e
Shaan <macbook@Amans-MacBook-Air.local>
2019-01-07 23:05:35 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from "react";
+import $ from "jquery";
+import Display from "./display";
+import "./App.css";
+
+window.jQuery = $;
+window.$ = $;
+
+require("jquery-ui-sortable");
+require("formBuilder");
+require("formBuilder/dist/form-render.min");
+
+const formData = [];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datatype: ""
+    };
+  }
+  form = createRef();
+  formRenderer = createRef();
+  componentDidMount() {
+    console.log(this.formRenderer);
+    console.log($(this.formRenderer.current));
+    this.formInstance = $(this.form.current).formBuilder({
+      formData,
+      onSave: (evt, data) => {
+        console.log(data);
+        $(this.formRenderer.current).formRender({ formData: data });
+      }
+    });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="form-editor" ref={this.form}>
+        <form id="form-render" ref={this.formRenderer} />
       </div>
     );
   }
---
+++ src/display.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Display extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+export default Display;
